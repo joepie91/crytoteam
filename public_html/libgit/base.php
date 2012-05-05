bcf3095df8f91f95cd6123cde8e83d1ef0c9ba0f
Sven Slootweg <jamsoftgamedev@gmail.com>
2012-05-05 20:10:55 +0200

    Moving pretty_dump elsewhere

--- public_html/libgit/base.php
+++ public_html/libgit/base.php
@@ -1,4 +1,23 @@
 <?php
+function pretty_dump($input)
+{
+	ob_start();
+	
+	var_dump($input);
+	
+	$output = ob_get_contents();
+	ob_end_clean();
+	
+	while(preg_match("/^[ ]*[ ]/m", $output) == 1)
+	{
+		$output = preg_replace("/^([ ]*)[ ]/m", "$1&nbsp;&nbsp;&nbsp;", $output);
+	}
+	
+	$output = nl2br($output);
+	
+	echo($output);
+}
+
 require(dirname(__FILE__) . "/class.repository.php");
 require(dirname(__FILE__) . "/class.object.php");
 require(dirname(__FILE__) . "/class.commit.php");
--- public_html/libgit/class.repository.php
+++ public_html/libgit/class.repository.php
@@ -1,24 +1,4 @@
 <?php
-function pretty_dump($input)
-{
-	ob_start();
-	
-	var_dump($input);
-	
-	$output = ob_get_contents();
-	ob_end_clean();
-	
-	while(preg_match("/^[ ]*[ ]/m", $output) == 1)
-	{
-		$output = preg_replace("/^([ ]*)[ ]/m", "$1&nbsp;&nbsp;&nbsp;", $output);
-	}
-	
-	$output = nl2br($output);
-	
-	echo($output);
-}
-
-
 class GitRepository
 {
 	public $path = "";
